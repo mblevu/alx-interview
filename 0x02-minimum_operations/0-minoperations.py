631f3657dcd495d92f56fd5f30632029f6b0ae6d
mblevu <brianislevu@gmail.com>
2024-01-18 11:14:05 +0300

    Update shebang and improve code formatting

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """the goal is to find the minimum number of operations
 needed to create exactly n 'H' characters in a text file"""
 
 
 def minOperations(n):
-    """Calculate the minimum number of operations required to reach a target number.
+    """Calculate the minimum number of operations required
+    to reach a target number.
 
     Args:
         n (int): The target number.
