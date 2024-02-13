2bbcd99396b763bc1e012fc8f7d10fa8876c51e1
mblevu <brianislevu@gmail.com>
2024-02-13 18:51:09 +0300

    Refactor character retrieval in Star Wars API script

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,18 +7,21 @@ const url = `https://swapi.dev/api/films/${movieId}/`;
 request(url, function (error, response, body) {
   if (error) {
     console.error('error:', error);
-  } else {
-    const film = JSON.parse(body);
-    const characterUrls = film.characters;
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, function (error, response, body) {
-        if (error) {
-          console.error('error:', error);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
+    return;
   }
+
+  const film = JSON.parse(body);
+  const characterUrls = film.characters;
+
+  characterUrls.forEach((characterUrl) => {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        console.error('error:', error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
 });
