c67bffe64e29863ecaac8a6d4ec5a97c0bc7760b
mblevu <brianislevu@gmail.com>
2024-02-13 20:14:04 +0300

    Refactor Star Wars character retrieval code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/' + process.argv[2];
-request(url, function (error, response, body) {
+movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, async function (error, response, body) {
   if (error) {
-    console.error(error);
-  } else {
-    console.log(JSON.parse(body).name);
+    console.error('error:', error);
   }
-}
-);
+  const movie = JSON.parse(body);
+  for (const character of movie.characters) {
+    const p = new Promise((resolve, reject) => {
+      request(character, function (error, response, body) {
+        if (error) {
+          reject(error);
+        }
+        resolve(JSON.parse(body).name);
+      });
+    });
+    try {
+      console.log(await p);
+    } catch (error) {
+      console.error('error:', error);
+    }
+  }
+})
